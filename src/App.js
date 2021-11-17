487e146eeaf6020f7593de04ee036a7c8c174286
Santiago Montoya <smonto67@gmail.com>
2021-11-17 10:14:40 -0500

    corrigiendo desde cero

--- src/App.js
+++ src/App.js
@@ -1,40 +1,9 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Dashboard from "./components/Dashboard";
-import Footer from "./components/Footer";
-import Home from "./components/Home";
-import Proyectos from "./components/Proyectos";
-import Usuarios from "./components/Usuarios";
-import Login from "./components/Login";
-import editarUsuario from "./components/editarUsuario";
-import EditarCurso from "./components/EditarCurso";
-import registroUsuario from "./components/registroUsuario";
 import './App.css';
-import Solicitudes from "./components/solicitudes";
-import NuevoProyecto from "./components/NuevoProyecto";
 
 function App() {
-
-
   return (
     <div className="App">
-      <Router>
-        <Switch>
-
-          <div>
-            <Dashboard/>
-            <Route path="/login" exact component={() => <Login />} />
-            <Route exact path="/registro/:nuevousuario" component={registroUsuario} />
-            <Route path="/nuevoProyecto" exact component={() => <NuevoProyecto />} />
-            <Route exact path="/editar_cursos/:id" component={EditarCurso} />
-            <Route path="/home" exact component={() => <Home />} />
-            <Route path="/proyectos" exact component={() => <Proyectos />} />
-            <Route path="/usuarios" exact component={() => <Usuarios />} />
-            <Route path="/Solicitudes" exact component={() => <Solicitudes />} />
-            <Route exact path="/usuarios/:id" component={editarUsuario} />
-            <Footer />
-          </div>
-        </Switch>
-      </Router>
+      <h1>Hola Mundo</h1>
     </div>
   );
 }
--- src/conexion-bd/funciones.js
+++
@@ -1,8 +0,0 @@
-const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://smonto:smonto123456@cluster0.qsznc.mongodb.net/SoulWeb?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
