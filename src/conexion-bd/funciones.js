47119f158c50a766eb7e20cf7b4a783e87eab5c6
Santiago Montoya <smonto67@gmail.com>
2021-11-17 09:37:21 -0500

    restaurando

--- src/conexion-bd/funciones.js
+++ src/conexion-bd/funciones.js
@@ -1,10 +1,8 @@
-// import { mongoose } from "mongoose";
-
-// const url = mongoose.connect("mongodb+srv://smonto:smonto123456@cluster0.qsznc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
-
-
-// mongoose.connect(url, {
-
-// })
-// .then(() => console.log("Conectado a Mongo"))
-// .catch((e) => console.log("El error de conexion es: "+ e))
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://smonto:smonto123456@cluster0.qsznc.mongodb.net/SoulWeb?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
