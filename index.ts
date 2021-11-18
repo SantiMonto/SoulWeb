dd428baa46d71552b5644ea7903b82b411b02764
Santiago Montoya <smonto67@gmail.com>
2021-11-18 11:30:13 -0500

    modelo usuario y creacion usuario

--- index.ts
+++ index.ts
@@ -1,7 +1,19 @@
 import conectarBD from "./db/db";
+import {UserModel} from "./modelos/user";
 
 const main = async () =>{
     await conectarBD();
+
+    await UserModel.create({
+        correo: "smonto@gmail.com",
+        identificacion: "123456",
+        nombre: "santiago",
+        apellido: "montoya"
+    }).then((u) => {
+        console.log("Usuario creado",u)
+    }).catch((e) => {
+        console.error("Error creando el usuario",e);
+    })
 }
 
 main();
