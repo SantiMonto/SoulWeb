9e4ac1eff3f8cc270c3b2eb26582779f81346cc1
Santiago Montoya <smonto67@gmail.com>
2021-11-19 09:52:08 -0500

    enum_role e interface

--- modelos/user.ts
+++ modelos/user.ts
@@ -1,6 +1,17 @@
 import {Schema, model} from 'mongoose';
+import { Enum_Rol } from './enums';
 
-const userSchema = new Schema({
+
+interface User{
+    correo: string;
+    identificacion: string;
+    nombre: string;
+    apellido: string;
+    rol: Enum_Rol;
+}
+
+
+const userSchema = new Schema <User>({
     correo:{
         type: String,
         required: true
@@ -17,6 +28,11 @@ const userSchema = new Schema({
     apellido:{
         type: String,
         required: true
+    },
+    rol: {
+        type: String,
+        required: true,
+        enum: Enum_Rol
     }
 })
 
