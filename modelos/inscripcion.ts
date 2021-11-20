aa18acda95208b1c14ff3d76f6cfd966f728e952
David Pérez <david.spr@hotmail.com>
2021-11-20 12:10:21 -0500

    Modelo Inscripcion.ts y enum

---
+++ modelos/inscripcion.ts
@@ -0,0 +1,40 @@
+import {Schema, model} from 'mongoose';
+import { Enum_EstadoInscripcion } from './enums';
+import { ProjectModel } from './project';
+import { UserModel } from './user';
+
+
+
+interface Inscripcion{
+
+    proyecto:Schema.Types.ObjectId;
+    estudiante:Schema.Types.ObjectId;
+    estado:Enum_EstadoInscripcion;
+    fechaInicio: Date;
+    fechaFin:Date;
+    
+}
+const inscripcionSchema = new Schema<Inscripcion>({
+    proyecto:{
+        type: Schema.Types.ObjectId,
+        ref: ProjectModel,        
+    },
+    estudiante:{
+        type: Schema.Types.ObjectId,
+        ref:UserModel,
+    },
+    estado:{
+        type: String,
+        enum: Enum_EstadoInscripcion,
+        required: true, 
+    },
+    fechaInicio:{
+        type:Date,
+        required:true,
+    },
+    fechaFin:{
+        type:Date,
+        required:true,
+    },
+});
+const InscripcionModel = model("Inscripcion", inscripcionSchema)
