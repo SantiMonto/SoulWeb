4e58f7afde2ef095cba4564523652b9b13343022
Santiago Montoya <smonto67@gmail.com>
2021-11-23 11:58:36 -0500

    Enum_EstadoUsuario.PENDIENTE para coincidir

--- modelos/user.ts
+++ modelos/user.ts
@@ -1,5 +1,5 @@
 import {Schema, model} from 'mongoose';
-import { Enum_Rol } from './enums';
+import { Enum_Rol, Enum_EstadoUsuario } from './enums';
 
 interface User{
     correo: string;
@@ -7,6 +7,7 @@ interface User{
     nombre: string;
     apellido: string;
     rol: Enum_Rol;
+    estado:Enum_EstadoUsuario;
 }
 
 const userSchema = new Schema <User>({
@@ -31,6 +32,11 @@ const userSchema = new Schema <User>({
         type: String,
         required: true,
         enum: Enum_Rol
+    },
+    estado:{
+        type: String,
+        enum: Enum_EstadoUsuario,
+        default: Enum_EstadoUsuario.PENDIENTE
     }
 })
 
