abf2b26c5ca268e15e6bfddcf05e6e052f0a7952
Santiago Montoya <smonto67@gmail.com>
2021-12-08 13:25:42 -0500

    encriptando contraseña con bcrypt

--- graphql/auth/resolvers.ts
+++ graphql/auth/resolvers.ts
@@ -1,16 +1,18 @@
 import { UserModel } from "../../modelos/usuario/user";
-
+import bcrypt from "bcrypt";
 
 const resolversAutenticacion = {
     Mutation: {
         registro: async (parent,args) =>{
+            const salt = await bcrypt.gensalt(10);
+            const hashedPassword = await bcrypt.hash();
             const usuarioCreado = await UserModel.create({
                 nombre: args.nombre,
                 apellido: args.apellido,
                 identificacion: args.identificacion,
                 correo: args.correo,
                 rol: args.rol,
-                password: args.password,
+                password: hashedPassword,
             });
             console.log('Usuario creado', usuarioCreado)
             return 'Usuario creado'
