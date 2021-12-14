566fb6a82cb1c9560c0c9dcd1946d1099c6202a5
Santiago Montoya <smonto67@gmail.com>
2021-12-14 09:52:10 -0500

    se agrega el crud de objtevos

--- modelos/proyecto/resolvers.js
+++ modelos/proyecto/resolvers.js
@@ -1,56 +1,97 @@
+import { InscripcionModel } from "../inscripcion/inscription.js";
+import { UserModel } from "../usuario/user.js";
 import { ProjectModel } from "./project.js"
 
 const resolversProject = {
-    Query: {
-        Proyectos: async (parent, args) => {
-            const proyectos = await ProjectModel.find().populate('lider').populate('inscripciones')
-            return proyectos
+    Proyecto: {
+        lider: async (parent, args, context) => {
+          const usr = await UserModel.findOne({
+            _id: parent.lider.toString(),
+          });
+          return usr;
         },
-
-        Proyecto: async (parent, args) => {
-            const proyecto = await ProjectModel.findOne({ _id: args._id })
-            return proyecto
-        }
-    },
-
-    Mutation: {
-        cambiarEstadoProyecto: async (parent, args) => {
-            const usuarioEditado = await ProjectModel.findByIdAndUpdate(
-                args._id,
-                {
-                    estado: 'ACTIVO'
+        inscripciones: async (parent, args, context) => {
+          const inscripciones = await InscripcionModel.find({
+            proyecto: parent._id,
+          });
+          return inscripciones;
+        },
+      },
+      Query: {
+        Proyectos: async (parent, args, context) => {
+          if (context.userData) {
+            if (context.userData.rol === 'LIDER') {
+              const proyectos = await ProjectModel.find({ lider: context.userData._id });
+              console.log('es lider de', proyectos);
+              return proyectos;
+            }
+          }
+          const proyectos = await ProjectModel.find();
+          return proyectos;
+        },
+      },
+      Mutation: {
+        crearProyecto: async (parent, args, context) => {
+          const proyectoCreado = await ProjectModel.create({
+            nombre: args.nombre,
+            fechaInicio: args.fechaInicio,
+            fechaFin: args.fechaFin,
+            presupuesto: args.presupuesto,
+            lider: args.lider,
+            objetivos: args.objetivos,
+          });
+          return proyectoCreado;
+        },
+        editarProyecto: async (parent, args) => {
+          const proyectoEditado = await ProjectModel.findByIdAndUpdate(
+            args._id,
+            { ...args.campos },
+            { new: true }
+          );
+    
+          return proyectoEditado;
+        },
+        crearObjetivo: async (parent, args) => {
+          const proyectoConObjetivo = await ProjectModel.findByIdAndUpdate(
+            args.idProyecto,
+            {
+              $addToSet: {
+                objetivos: { ...args.campos },
+              },
+            },
+            { new: true }
+          );
+    
+          return proyectoConObjetivo;
+        },
+        editarObjetivo: async (parent, args) => {
+          const proyectoEditado = await ProjectModel.findByIdAndUpdate(
+            args.idProyecto,
+            {
+              $set: {
+                [`objetivos.${args.indexObjetivo}.descripcion`]: args.campos.descripcion,
+                [`objetivos.${args.indexObjetivo}.tipo`]: args.campos.tipo,
+              },
+            },
+            { new: true }
+          );
+          return proyectoEditado;
+        },
+        eliminarObjetivo: async (parent, args) => {
+          const proyectoObjetivo = await ProjectModel.findByIdAndUpdate(
+            { _id: args.idProyecto },
+            {
+              $pull: {
+                objetivos: {
+                  _id: args.idObjetivo,
                 },
-                { new: true })
-                return usuarioEditado
-        },
-        actEstadoProyecto: async (parent, args) => {
-            const cambioEstado = await ProjectModel.findByIdAndUpdate(args._id,{
-                estado: 'INACTIVO'
+              },
             },
-            {new: true})
-            return cambioEstado
+            { new: true }
+          );
+          return proyectoObjetivo;
         },
-        actFaseProyecto: async (parent, args) => {
-            const cambioFase = await ProjectModel.findByIdAndUpdate(args._id, {
-                fase: 'TERMINADO'
-            },
-            {new:true})
-            return cambioFase
-        },
-        crearProyecto: async (parent, args) => {
-            const proyectoNuevo = await ProjectModel.create({
-                nombre: args.nombre,
-                presupuesto: args.presupuesto,
-                fechaInicio: args.fechaInicio,
-                fechaFin: args.fechaFin,
-                estado: args.estado,
-                fase: args.fase,
-                lider: args.lider,
-                objetivos: args.objetivos
-            });
-            return proyectoNuevo;
-        }
-    }
-}
+      },
+    };
 
 export { resolversProject }
--- modelos/proyecto/types.js
+++ modelos/proyecto/types.js
@@ -6,15 +6,25 @@ type Objetivo {
     _id: ID!
     descripcion: String!
     tipo: Enum_TipoObjetivo!
-}
-
-input crearObjetivo{
-    
+  }
+  input crearObjetivo {
     descripcion: String!
     tipo: Enum_TipoObjetivo!
-}
-
-type Proyecto {
+  }
+  input camposObjetivo {
+    descripcion: String!
+    tipo: Enum_TipoObjetivo!
+  }
+  input camposProyecto {
+    nombre: String
+    presupuesto: Float
+    fechaInicio: Date
+    fechaFin: Date
+    estado: Enum_EstadoProyecto
+    fase: Enum_FaseProyecto
+    lider: String
+  }
+  type Proyecto {
     _id: ID!
     nombre: String!
     presupuesto: Float!
@@ -24,45 +34,26 @@ type Proyecto {
     fase: Enum_FaseProyecto!
     lider: Usuario!
     objetivos: [Objetivo]
-    inscripcion: [Inscripcion]
-}
-
-type Query {
+    avances: [Avance]
+    inscripciones: [Inscripcion]
+  }
+  type Query {
     Proyectos: [Proyecto]
-    Proyecto(_id: String!): Proyecto
-}
-
-type Mutation {
-    cambiarEstadoProyecto(
-        _id: String!
-        estado: Enum_EstadoProyecto!
-    ): Proyecto
-
-
-    actEstadoProyecto(
-        _id: String!
-        estado: Enum_EstadoProyecto!
-
-    ): Proyecto
-
-    actFaseProyecto(
-        _id: String!
-        fase: Enum_FaseProyecto!
-    ): Proyecto
-
+  }
+  type Mutation {
     crearProyecto(
-        nombre: String!
-        presupuesto: Float!
-        fechaInicio: Date!
-        fechaFin: Date!
-        estado: Enum_EstadoProyecto!
-        fase: Enum_FaseProyecto!
-        lider: String!
-        objetivos: [crearObjetivo]
+      nombre: String!
+      presupuesto: Float!
+      fechaInicio: Date!
+      fechaFin: Date!
+      lider: String!
+      objetivos: [crearObjetivo]
     ): Proyecto
-    
-
-}
+    editarProyecto(_id: String!, campos: camposProyecto!): Proyecto
+    crearObjetivo(idProyecto: String!, campos: camposObjetivo!): Proyecto
+    editarObjetivo(idProyecto: String!, indexObjetivo: Int!, campos: camposObjetivo!): Proyecto
+    eliminarObjetivo(idProyecto: String!, idObjetivo: String!): Proyecto
+  }
 
 `;
 
